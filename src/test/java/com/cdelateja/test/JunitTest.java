8de23bdb70adbac0ded478f3ee2424f83ad34f39
wterry <wterry@legosoft.com.mx>
2021-03-23 15:55:14 -0600

    se hardcodea la variable del test. Para prueba de ejecucion nada mas

--- src/test/java/com/cdelateja/test/JunitTest.java
+++ src/test/java/com/cdelateja/test/JunitTest.java
@@ -3,13 +3,13 @@ package com.cdelateja.test;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class JunitTest {
 
     @Test
     public void justAnExample() {
-        String variable = System.getenv("DISABLE_AUTH");
+        String variable = "true";
         System.out.println(variable);
         assertEquals("true", variable);
     }
